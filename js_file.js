936f13fae97f97daafcfb48201bdd9cb81c65dba
kiran raveendran <kiranraveendran50@gmail.com>
2024-04-06 17:54:01 +0530

    Made some modificaton

--- js_file.js
+++ js_file.js
@@ -3,27 +3,27 @@ let slider = document.getElementById('slider');
 let cards = slider.getElementsByTagName('li');
 
 let elementsToshow = 3;
-//if (document.body.clientWidth < 300) {
+// if (document.body.clientWidth < 300) {
 //    elementsToshow = 1;
-//} else if (document.body.clientWidth < 700) {
+// } else if (document.body.clientWidth < 700) {
 //    elementsToshow = 2;
-//} else {
+// } else {
 //    elementsToshow=3;
-//}
+// }
 
 let sliderContainerwidth = sliderContainer.clientWidth;
 //clientWidth will return the value of the width of the corrently pointing element body.
 
 let cardwidth = sliderContainerwidth / elementsToshow;
 
-slider.style.width = cards.length * cardwidth + 'px';
+slider.style.width = cards.length * cardwidth +'px';
 slider.style.transition = 'margin';
 slider.style.transitionDuration = '1s';
 
 
 for (let index = 0; index < cards.length; index++){
     const element = cards(index);
-    element.style.width = cardwidth + 'px';
+    element.style.width = cardwidth+ 'px';
 }
 
 function Prev() {
@@ -40,10 +40,10 @@ function Next() {
 }
 
 
-// const infiniteScroll = document.querySelector('.infinite-scroll');
+const infiniteScroll = document.querySelector('.infinite-scroll');
 
-// infiniteScroll.addEventListener('scroll', function() {
-//   if (infiniteScroll.scrollLeft === infiniteScroll.scrollWidth) {
-//     infiniteScroll.scrollLeft = 0;
-//   }else{} 
-// });
+infiniteScroll.addEventListener('scroll', function() {
+  if (infiniteScroll.scrollLeft === infiniteScroll.scrollWidth) {
+    infiniteScroll.scrollLeft = 0;
+  }else{} 
+});
